4e111d1f0fdfcea2c4ba6938233764c6b5b73d95
Manav Chaudhari <vrmanav02@gmail.com>
2025-03-29 17:54:48 +0530

    Complete excercises

---
+++ Days1-10/Day04/excercise.py
@@ -0,0 +1,13 @@
+
+# * Learning about lists
+# fruits = ["Apple", "Peach", "Pear"]
+# print(fruits[1])
+# print(fruits[-2])
+# for fruit in fruits:
+#     print(fruit)
+
+# * Who pays the bill
+# import random
+# friends = ["Rahul", "Rajesh", "Rohan", "Raj"]
+# random_index = random.randint(0, 3)
+# print(f"{friends[random_index]} will pay the bill")
