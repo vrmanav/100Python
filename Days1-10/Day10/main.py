23363ae9a87f79b82e02f4d68f07521f2bd9cfce
Manav Chaudhari <vrmanav02@gmail.com>
2025-04-04 13:44:08 +0530

    Calculator

--- Days1-10/Day10/main.py
+++ Days1-10/Day10/main.py
@@ -29,30 +29,43 @@ symbols = {
 
 
 def calculator():
+    os.system("clear")
     print(art.logo)
     time.sleep(1.5)
     os.system("clear")
-
     while True:
         try:
             num1 = float(input("\nEnter your first number: "))
             break
         except ValueError:
             print("⚠️  You have entered an invalid input, type in a numerical value ⚠️")
-    print()
-    for symbol in symbols:
-        print(symbol, end=" ")
-    operation = input("\nWhat operation do you want to perform: ")
-    while True:
-        try:
-            num2 = float(input("\nEnter your second number: "))
-            break
-        except ValueError:
-            print("⚠️  You have entered an invalid input, type in a numerical value ⚠️")
 
-    operation_function = symbols[operation]
-    result = operation_function(num1, num2)
-    print(f"\n{num1} {operation} {num2} = {result}")
+    do_continue = True
+    while do_continue:
+        print()
+        for symbol in symbols:
+            print(symbol, end=" ")
+        operation = input("\nWhat operation do you want to perform: ")
+        while True:
+            try:
+                num2 = float(input("\nEnter the next number: "))
+                break
+            except ValueError:
+                print(
+                    "⚠️  You have entered an invalid input, type in a numerical value ⚠️"
+                )
+        operation_function = symbols[operation]
+        result = operation_function(num1, num2)
+        print(f"\n{num1} {operation} {num2} = {result}")
+
+        choice = input(
+            f"Do you want to continue with {result} or want to start a fresh calculation [NEW/CONTINUE]: "
+        ).lower()
+        if choice == "continue":
+            num1 = result
+        elif choice == "new":
+            calculator()
+            do_continue = False
 
 
 calculator()
