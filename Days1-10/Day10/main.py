ec09e7623ebdc227386c4237ebfe5671a4a4480f
Manav Chaudhari <vrmanav02@gmail.com>
2025-04-04 13:51:35 +0530

    Calculator
    Optimze code

--- Days1-10/Day10/main.py
+++ Days1-10/Day10/main.py
@@ -5,18 +5,24 @@ import time
 
 
 def add(num1, num2):
+    """Adds two numbers."""
     return num1 + num2
 
 
 def subtract(num1, num2):
+    """Subtracts two numbers."""
     return num1 - num2
 
 
 def multiply(num1, num2):
+    """Multiplies two numbers."""
     return num1 * num2
 
 
 def divide(num1, num2):
+    """Divides two numbers."""
+    if num2 == 0:
+        return "Cannot divide by zero"
     return num1 / num2
 
 
@@ -29,10 +35,10 @@ symbols = {
 
 
 def calculator():
+    """Runs the calculator program."""
     os.system("clear")
     print(art.logo)
-    time.sleep(1.5)
-    os.system("clear")
+    time.sleep(1)
     while True:
         try:
             num1 = float(input("\nEnter your first number: "))
@@ -40,32 +46,33 @@ def calculator():
         except ValueError:
             print("⚠️  You have entered an invalid input, type in a numerical value ⚠️")
 
-    do_continue = True
-    while do_continue:
-        print()
+    continue_calculation = True
+    while continue_calculation:
+        print("\nAvailable operations: ", end="")
         for symbol in symbols:
             print(symbol, end=" ")
         operation = input("\nWhat operation do you want to perform: ")
+        if operation not in symbols:
+            print("⚠️ Invalid input. Please enter a numerical value ⚠️")
+            continue
         while True:
             try:
                 num2 = float(input("\nEnter the next number: "))
                 break
             except ValueError:
-                print(
-                    "⚠️  You have entered an invalid input, type in a numerical value ⚠️"
-                )
+                print("⚠️ Invalid input. Please enter a numerical value ⚠️")
         operation_function = symbols[operation]
         result = operation_function(num1, num2)
         print(f"\n{num1} {operation} {num2} = {result}")
 
         choice = input(
-            f"Do you want to continue with {result} or want to start a fresh calculation [NEW/CONTINUE]: "
+            f"Type 'yes' to continue calculating with {result}, or type 'no' to start a new calculation: "
         ).lower()
-        if choice == "continue":
+        if choice == "yes":
             num1 = result
-        elif choice == "new":
+        elif choice == "no":
+            continue_calculation = False
             calculator()
-            do_continue = False
 
 
 calculator()
