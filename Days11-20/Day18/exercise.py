c9521d4a2775c7d140e7350060f9858f9989f783
Manav Chaudhari <vrmanav02@gmail.com>
2025-04-10 00:41:26 +0530

    Hirst painting

---
+++ Days11-20/Day18/exercise.py
@@ -0,0 +1,29 @@
+from turtle import Turtle, Screen
+import random
+
+tim = Turtle("turtle")
+
+#! Draw a square
+# for _ in range(4):
+#     tim.forward(200)
+#     tim.left(90)
+
+#! Draw dotted line
+# for _ in range(10):
+#     tim.forward(10)
+#     tim.penup()
+#     tim.forward(10)
+#     tim.pendown()
+
+#! Draw different shapes
+# def draw_shape(sides):
+#     angle = 360 / sides
+#     for _ in range(side):
+#         tim.forward(100)
+#         tim.right(angle)
+# for side in range(3, 11):
+#     draw_shape(side)
+
+
+screen = Screen()
+screen.exitonclick()
