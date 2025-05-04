05b83fd276ab69d0f3107bfd4029f1853d7e27c8
Manav Chaudhari <vrmanav02@gmail.com>
2025-05-04 18:16:35 +0530

    Mail Merge
    Update letter generation logic

--- Days21-30/Day24/main.py
+++ Days21-30/Day24/main.py
@@ -1,24 +1,12 @@
-# TODO: Create a letter using starting_letter.txt
-# for each name in invited_names.txt
-# Replace the [name] placeholder with the actual name.
-# Save the letters in the folder "ReadyToSend".
+PLACEHOLDER = "[name]"
 
-# Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
-# Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
-# Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
-
-
-formatted_list = []
-
-with open("./invitees.txt", mode="r") as invitees:
-    original_list = invitees.readlines()
-    for person in original_list:
-        name = person.strip()
-        formatted_list.append(name)
+with open("./invitees.txt", mode="r") as guest_names:
+    names = guest_names.readlines()
 
 with open("./format.txt", mode="r") as format:
     letter_str = format.read()
-    for name in formatted_list:
-        letter = letter_str.replace("[name]", name)
+    for name in names:
+        formatted_name = name.strip()
+        letter = letter_str.replace("[name]", formatted_name)
         with open(f"./ReadyToSend/letter_for_{name}", mode="w") as final_letter:
             final_letter.write(letter)
